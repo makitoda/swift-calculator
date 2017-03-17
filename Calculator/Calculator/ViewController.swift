5768bccad4ed2c0ef628ddbd1f0d75c240d95e94
maki toda <earth.in.us@gmail.com>
2017-03-17 13:28:16 -0700

    created project.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 戸田 真紀 on 2017-03-17.
+//  Copyright © 2017 Individual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
